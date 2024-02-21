2adaad557e18e6f8e2c150b6006260daf4945062
Jero Ginaca <108022277+jeroginaca@users.noreply.github.com>
2024-02-21 17:22:09 +0100

    contact-page

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,17 @@
+import Contact2 from '@/components/sections/Contact2'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        <Contact2 />
+    </div>
+  )
+}
+
+export default page
